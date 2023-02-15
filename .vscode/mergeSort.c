8647545ead3ccf0252041b25af5385fa17c15780
George <84743235+nikibeep@users.noreply.github.com>
2023-02-15 13:26:12 +0530

    new file:   .vscode/mergeSort.c 	new file:   .vscode/mergeSort.exe 	modified:   .vscode/sorting.c

---
+++ .vscode/mergeSort.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Create temporary arrays
+    int L[n1], R[n2];
+
+    // Copy data to temporary arrays
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temporary arrays back into arr[l..r]
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        // Find the middle point
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        // Merge the sorted halves
+        merge(arr, l, m, r);
+
+        // Print the intermediate steps
+        printf("Intermediate array: ");
+        for (int i = l; i <= r; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int arr[] = {9, 6, 5, 0, 8, 2, 7, 1, 3, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    mergeSort(arr, 0, n - 1);
+
+    // Validate the sorted array
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+        if (i > 0 && arr[i] < arr[i - 1]) {
+            printf("\nValidation error: Array is not sorted!");
+            return 1;
+        }
+    }
+    printf("\n");
+
+    printf("Validation successful: Array is sorted!");
+    return 0;
+}
--- .vscode/sorting.c
+++ .vscode/sorting.c
@@ -84,6 +84,51 @@
             printf("%d ",a[i]);
         }
     }
+    //Merge sort with steps
+    int merge(int a[],int l,int m,int r)
+    {
+        int i,j,k;
+        int n1=m-l+1;
+        int n2=r-m;
+        int L[n1],R[n2];
+        for(i=0;i<n1;i++)
+        {
+            L[i]=a[l+i];
+        }
+        for(j=0;j<n2;j++)
+        {
+            R[j]=a[m+1+j];
+        }
+        i=0;
+        j=0;
+        k=l;
+        while(i<n1 && j<n2)
+        {
+            if(L[i]<=R[j])
+            {
+                a[k]=L[i];
+                i++;
+            }
+            else
+            {
+                a[k]=R[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<n1)
+        {
+            a[k]=L[i];
+            i++;
+            k++;
+        }
+        while(j<n2)
+        {
+            a[k]=R[j];
+            j++;
+            k++;
+        }
+    }
 void main()
 
 {   
@@ -112,7 +157,9 @@ void main()
                 break;
         case 3: selection(a,n);
                 break;
-        case 4: exit(0);
+        case 4: merge(a,0,n/2,n-1);
+                break;
+        case 5: exit(0);
                 break;
         default: printf("Invalid choice");
     }
