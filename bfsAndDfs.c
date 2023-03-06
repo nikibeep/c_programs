cccc8b4b145f24cf816220782087babe3971377e
George <84743235+nikibeep@users.noreply.github.com>
2023-03-06 15:20:18 +0530

    modified:   bfsAndDfs.c 	deleted:    bfsAndDfs.exe 	new file:   bfsAndDfs2.exe

--- bfsAndDfs.c
+++ bfsAndDfs.c
@@ -1,109 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 100
+#define MAX_VERTICES 26
 
-// Queue data structure
-typedef struct queue {
-    int items[MAX];
-    int front;
-    int rear;
-} queue;
-
-// Stack data structure
-typedef struct stack {
-    int items[MAX];
-    int top;
-} stack;
-
-// Matrix data structure
-int matrix[MAX][MAX];
-int visited[MAX];
+int matrix[MAX_VERTICES][MAX_VERTICES];
+int visited[MAX_VERTICES];
 int n;
 
-// Queue operations
-void createQueue(queue *q) {
-    q->front = -1;
-    q->rear = -1;
-}
+typedef struct QueueNode {
+    int data;
+    struct QueueNode* next;
+} QueueNode;
 
-int isEmpty(queue *q) {
-    if (q->rear == -1)
-        return 1;
-    else
-        return 0;
-}
+typedef struct Queue {
+    QueueNode *front, *rear;
+} Queue;
 
-void enqueue(queue *q, int value) {
-    if (q->rear == MAX - 1)
-        printf("Queue is full\n");
-    else {
-        if (q->front == -1)
-            q->front = 0;
-        q->rear++;
-        q->items[q->rear] = value;
+typedef struct StackNode {
+    int data;
+    struct StackNode* next;
+} StackNode;
+
+typedef struct Stack {
+    StackNode* top;
+} Stack;
+
+void enqueue(Queue* q, int data) {
+    QueueNode* newNode = (QueueNode*)malloc(sizeof(QueueNode));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (q->rear == NULL) {
+        q->front = q->rear = newNode;
+        return;
     }
+
+    q->rear->next = newNode;
+    q->rear = newNode;
 }
 
-int dequeue(queue *q) {
-    int item;
-    if (isEmpty(q)) {
-        printf("Queue is empty\n");
-        item = -1;
-    } else {
-        item = q->items[q->front];
-        q->front++;
-        if (q->front > q->rear) {
-            q->front = q->rear = -1;
-        }
+int dequeue(Queue* q) {
+    if (q->front == NULL) {
+        return -1;
     }
-    return item;
-}
 
-// Stack operations
-void createStack(stack *s) {
-    s->top = -1;
-}
+    int data = q->front->data;
+    QueueNode* temp = q->front;
 
-int isStackEmpty(stack *s) {
-    if (s->top == -1)
-        return 1;
-    else
-        return 0;
-}
+    q->front = q->front->next;
 
-void push(stack *s, int value) {
-    if (s->top == MAX - 1)
-        printf("Stack is full\n");
-    else {
-        s->top++;
-        s->items[s->top] = value;
+    if (q->front == NULL) {
+        q->rear = NULL;
     }
+
+    free(temp);
+    return data;
 }
 
-int pop(stack *s) {
-    int item;
-    if (isStackEmpty(s)) {
-        printf("Stack is empty\n");
-        item = -1;
-    } else {
-        item = s->items[s->top];
-        s->top--;
+int isQueueEmpty(Queue* q) {
+    return (q->front == NULL);
+}
+
+void push(Stack* s, int data) {
+    StackNode* newNode = (StackNode*)malloc(sizeof(StackNode));
+    newNode->data = data;
+    newNode->next = s->top;
+
+    s->top = newNode;
+}
+
+int pop(Stack* s) {
+    if (s->top == NULL) {
+        return -1;
     }
-    return item;
+
+    int data = s->top->data;
+    StackNode* temp = s->top;
+
+    s->top = s->top->next;
+
+    free(temp);
+    return data;
 }
 
-// Breadth First Search (BFS)
-void bfs(int vertex) {
-    queue q;
-    createQueue(&q);
+int isStackEmpty(Stack* s) {
+    return (s->top == NULL);
+}
 
-    visited[vertex] = 1;
-    printf("%c ", vertex + 'A');
+void bfs(int start) {
+    Queue q = { NULL, NULL };
 
-    enqueue(&q, vertex);
+    visited[start] = 1;
+    printf("Traversal order: %c ", start + 'A');
+    enqueue(&q, start);
 
-    while (!isEmpty(&q)) {
+    while (!isQueueEmpty(&q)) {
         int v = dequeue(&q);
 
         for (int i = 0; i < n; i++) {
@@ -114,17 +105,22 @@ void bfs(int vertex) {
             }
         }
     }
+
+    // Display traversed vertices alphabetically
+    printf("\nTraversed vertices alphabetically: ");
+    for (int i = 0; i < n; i++) {
+        if (visited[i] == 1) {
+            printf("%c ", i + 'A');
+        }
+    }
 }
 
-// Depth First Search (DFS)
-void dfs(int vertex) {
-    stack s;
-    createStack(&s);
+void dfs(int start) {
+    Stack s = { NULL };
 
-    visited[vertex] = 1;
-    printf("%c ", vertex + 'A');
-
-    push(&s, vertex);
+    visited[start] = 1;
+    printf("Traversal order: %c ", start + 'A');
+    push(&s, start);
 
     while (!isStackEmpty(&s)) {
         int v = pop(&s);
@@ -137,12 +133,22 @@ void dfs(int vertex) {
             }
         }
     }
+
+// Display traversed vertices alphabetically
+printf("\nTraversed vertices alphabetically: ");
+for (int i = 0; i < n; i++) {
+    if (visited[i] == 1) {
+        printf("%c ", i + 'A');
+    }
+}
 }
 
 int main() {
-    printf("Enter the number of vertices: ");
-    scanf("%d", &n);
-    // Input matrix
+int choice, start;
+char vertex;
+printf("Enter the number of vertices: ");
+scanf("%d", &n);
+
 printf("Enter the adjacency matrix:\n");
 for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
@@ -150,37 +156,33 @@ for (int i = 0; i < n; i++) {
     }
 }
 
-// Display matrix
-printf("The adjacency matrix is:\n");
-for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-        printf("%d ", matrix[i][j]);
+while (1) {
+    printf("\n1. Breadth First Search (BFS)\n2. Depth First Search (DFS)\n3. Exit\nEnter your choice: ");
+    scanf("%d", &choice);
+
+    if (choice == 3) {
+        break;
     }
-    printf("\n");
+
+    printf("Enter starting vertex (A-%c): ", 'A' + n - 1);
+    scanf(" %c", &vertex);
+
+    start = vertex - 'A';
+    // Initialize visited array
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+
+    if (choice == 1) {
+        bfs(start);
+    }
+    else if (choice == 2) {
+        dfs(start);
+    }
+    else {
+        printf("Invalid choice\n");
+    }
 }
-
-// Display BFS and DFS traversal
-printf("Enter the starting vertex for traversal: ");
-int vertex;
-scanf("%d", &vertex);
-
-// Initialize visited array to 0
-for (int i = 0; i < n; i++) {
-    visited[i] = 0;
-}
-
-printf("BFS traversal: ");
-bfs(vertex);
-printf("\n");
-
-// Reset visited array to 0
-for (int i = 0; i < n; i++) {
-    visited[i] = 0;
-}
-
-printf("DFS traversal: ");
-dfs(vertex);
-printf("\n");
 
 return 0;
 }
